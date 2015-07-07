26871a55f4571c3af925d579e777fb19f8f548b3
Björn Harrtell <bjorn@wololo.org>
2015-07-07 15:25:01 +0200

    Notes about how to calc tilegrid from pixel resolutions

--- src/main/scala/org/wololo/vectorvictor/VectorVictor.scala
+++ src/main/scala/org/wololo/vectorvictor/VectorVictor.scala
@@ -4,7 +4,7 @@ import java.io._
 import com.zaxxer.hikari._
 import resource._
 import scala.collection.parallel._
-import com.typesafe.scalalogging.slf4j.LazyLogging
+import com.typesafe.scalalogging.LazyLogging
 
 case class Extent(val minx: Double, miny: Double, maxx: Double, maxy: Double) {
   def width = maxx - minx
@@ -12,8 +12,15 @@ case class Extent(val minx: Double, miny: Double, maxx: Double, maxy: Double) {
   def toBBOX = List(minx, miny, maxx, maxy).mkString(",")
 }
 
+// How to find out max resolution from extent:
+// Math.ceil(Math.log2(distance))
+// How to select an extent that is divisable with resolution
+// Math.ceil(origo/resolution)*resolution
+
+
 case class GridExtent(extent: Extent, level: Int) {
   val tileSize = extent.height / math.pow(2, level)
+  //val tileSize = 256
   val minx = 0
   val miny = 0
   val maxx = Math.ceil(extent.width / tileSize).toInt - 1
@@ -31,12 +38,28 @@ object VectorVictor extends App with LazyLogging {
   val config = new HikariConfig("hikari.properties")
   val ds = new HikariDataSource(config)
   
+  var connection: java.sql.Connection = null
+  var statement: java.sql.Statement = null
+ 
+  try {
+    connection = ds.getConnection
+    statement = connection.createStatement()
+    statement.executeUpdate("truncate t_vv")
+  } finally {
+    statement.close()
+    connection.close()
+  }
+  
   val extent = Extent(218128, 6126002, 1083427, 7692850)
   
   val levels = Array(
     GridExtent(extent, 0),
+    GridExtent(extent, 1),
     GridExtent(extent, 2),
-    GridExtent(extent, 4)
+    GridExtent(extent, 3),
+    GridExtent(extent, 4),
+    GridExtent(extent, 5),
+    GridExtent(extent, 6)
   )
   
   def makeTiles(level: Int) = {
@@ -50,8 +73,7 @@ object VectorVictor extends App with LazyLogging {
     logger.info(s"Using tilesize " + grid.tileSize)
     
     var funcs = for (x <- grid.rx; y <- grid.ry) yield () => {
-      val bytes = fetchTile(grid.tileExtent(x,y).toBBOX);
-      
+      val bytes = fetchTile(grid.tileExtent(x,y).toBBOX)
       if (bytes != null) storeTile(bytes, x, y, level)
     }
     
@@ -59,7 +81,7 @@ object VectorVictor extends App with LazyLogging {
     
     parFuncs.tasksupport = new ForkJoinTaskSupport(new scala.concurrent.forkjoin.ForkJoinPool(20))
     
-    funcs.foreach(f => f())
+    parFuncs.foreach(f => f())
   }
   
   def storeTile(tile: Array[Byte], x: Int, y: Int, z: Int) = {
@@ -74,9 +96,12 @@ object VectorVictor extends App with LazyLogging {
     logger.info(s"Fetching tile for " + bbox)
     
     val envelope = s"ST_MakeEnvelope(${bbox}, 3006)";
-    var insert = s"insert into t_vv select gid from lantmateriet.ak_riks left join t_vv on gid != t_vv.id where geom @ ${envelope}";
-    var select = s"select ST_AsTWKB(array_agg(geom), array_agg(gid)) as geom from lantmateriet.ak_riks left join t_vv on gid != t_vv.id where geom @ ${envelope}";
+    var insert = s"insert into t_vv select gid from lantmateriet.ak_riks left join t_vv on gid = id where geom @ ${envelope} and id is null";
+    var select = s"select ST_AsTWKB(array_agg(geom), array_agg(gid)) as geom from (select gid, geom from lantmateriet.ak_riks left join t_vv on id = gid where geom @ ${envelope} and id is null) as q";
+    //var select = s"select ST_AsTWKB(array_agg(geom), array_agg(gid)) as geom from lantmateriet.ak_riks where geom @ ${envelope}";
 
+    var result: Array[Byte] = null
+    
     var connection: java.sql.Connection = null
     var statement: java.sql.Statement = null
     var resultSet: java.sql.ResultSet = null
@@ -86,9 +111,9 @@ object VectorVictor extends App with LazyLogging {
       statement = connection.createStatement()
       resultSet = statement.executeQuery(select)
       resultSet.next
-      resultSet.getBytes(1)
+      result = resultSet.getBytes(1)
     } finally {
-      resultSet.close()
+      if (resultSet != null) resultSet.close()
       statement.close()
       connection.close()
     }
@@ -97,13 +122,17 @@ object VectorVictor extends App with LazyLogging {
       connection = ds.getConnection
       statement = connection.createStatement()
       statement.executeUpdate(insert)
-      null
+      result
     } finally {
       statement.close()
       connection.close()
     }
   }
   
+  makeTiles(6)
+  makeTiles(5)
+  makeTiles(4)
+  makeTiles(3)
   makeTiles(2)
   makeTiles(1)
   makeTiles(0)

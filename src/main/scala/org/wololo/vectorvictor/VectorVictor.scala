da8b2a449653eb8f37ca8e38a7a8bb40c644187a
Björn Harrtell <bjorn@wololo.org>
2015-07-08 11:11:53 +0200

    Record visited indexes

--- src/main/scala/org/wololo/vectorvictor/VectorVictor.scala
+++ src/main/scala/org/wololo/vectorvictor/VectorVictor.scala
@@ -20,23 +20,26 @@ case class Grid(extent: Extent) extends LazyLogging {
   val maxResolution = (math.pow(2, math.ceil(math.log(maxDistance) / math.log(2))) / tileSize).toInt
   logger.info("Calculated maxResolution " + maxResolution)
   
-  val bounds = Extent(
-      (Math.ceil(extent.minx / maxResolution) * maxResolution).toInt,
-      (Math.ceil(extent.miny / maxResolution) * maxResolution).toInt,
-      (Math.ceil(extent.minx / maxResolution) * maxResolution + tileSize * maxResolution).toInt,
-      (Math.ceil(extent.miny / maxResolution) * maxResolution + tileSize * maxResolution).toInt
-  )
+  val bounds = calcBounds()
   
   logger.info("TileGrid total bounds " + bounds)
   
-  def resolution(level: Int) = (maxResolution/math.pow(2, level)).toInt
-  def range(level: Int) = 0 to math.pow(2, level).toInt-1
+  def resolution(level: Int) = (maxResolution / math.pow(2, level)).toInt
+  def range(level: Int) = 0 to math.pow(2, level).toInt - 1
   
-  def tileExtent(x:Int, y:Int, level: Int) : Extent = {
+  def tileExtent(x: Int, y: Int, level: Int) : Extent = {
     val w = tileSize * resolution(level)
     val minx = bounds.minx + w * x
     val miny = bounds.miny + w * y
     Extent(minx, miny, minx + w, miny + w)
+  }
+  
+  def calcBounds() : Extent = {
+    var minx = Math.ceil(extent.minx / maxResolution) * maxResolution
+    var miny = Math.ceil(extent.miny / maxResolution) * maxResolution
+    var maxx = minx + tileSize * maxResolution
+    var maxy = miny + tileSize * maxResolution
+    Extent(minx.toInt, miny.toInt, maxx.toInt, maxy.toInt)
   }
 }
 
@@ -55,9 +58,12 @@ object VectorVictor extends App with LazyLogging {
   var resolutions = List[Int]()
   var zs = List[Int]()
   var z = 2
+  var bitmaps = List[List[Int]]()
     
   def makeTiles(level: Int) = {
     logger.info("Making tiles for level " + level)
+    
+    var bitmap = List[Int]()
     
     resolutions = resolutions :+ grid.resolution(level)
     zs = zs :+ z
@@ -68,6 +74,7 @@ object VectorVictor extends App with LazyLogging {
       val bytes = fetchTile(grid.tileExtent(x, y, level))
       if (!bytes.isEmpty) {
         storeTile(bytes.get, x, y)
+        bitmap = bitmap :+ y * r.length + x
         c += 1
       }
     }
@@ -77,6 +84,8 @@ object VectorVictor extends App with LazyLogging {
     parFuncs.tasksupport = new ForkJoinTaskSupport(new scala.concurrent.forkjoin.ForkJoinPool(20))
     
     parFuncs.foreach(f => f())
+    
+    bitmaps = bitmaps :+ bitmap
     
     z -= 1
     
@@ -90,14 +99,13 @@ object VectorVictor extends App with LazyLogging {
     fos.write(tile, 0, tile.length)
     fos.close
   }
-  
-  def sqlEnvelope(extent: Extent) = sqls"ST_MakeEnvelope(${extent.minx}, ${extent.miny}, ${extent.maxx}, ${extent.maxy}, 3006)"
-  
+    
   def fetchTile(extent: Extent) : Option[Array[Byte]] = DB.autoCommit { implicit session =>
     logger.debug(s"Fetching tile for " + extent)
-    val envelope = sqlEnvelope(extent)
-    val bytes = sql"select ST_AsTWKB(array_agg(geom), array_agg(gid)) as geom from (select gid, geom from lantmateriet.ak_riks left join t_vv on id = gid where geom @ ${envelope} and id is null) as q".map(rs => rs.bytes(1)).single.apply()
-    sql"insert into t_vv select gid from lantmateriet.ak_riks left join t_vv on gid = id where geom @ ${envelope} and id is null".update.apply()
+    val envelope = sqls"ST_MakeEnvelope(${extent.minx}, ${extent.miny}, ${extent.maxx}, ${extent.maxy}, 3006)"
+    val select = sqls"select gid, geom from lantmateriet.ak_riks left join t_vv on id = gid where geom @ ${envelope} and id is null"
+    val bytes = sql"select ST_AsTWKB(array_agg(geom), array_agg(gid)) as geom from (${select}) as q".map(rs => rs.bytes(1)).single.apply()
+    sql"insert into t_vv select gid from (${select}) as q".update.apply()
     bytes
   }
   
@@ -106,6 +114,8 @@ object VectorVictor extends App with LazyLogging {
   makeTiles(0)
   
   // TODO: Output array representing "bitmap" of generated tiles
+  //logger.info("Bitmaps: " + bitmaps.reverse)
+      
   logger.info("Origin: " + grid.bounds.minx + ", " + grid.bounds.miny)
   logger.info("Resolutions: " + resolutions.reverse.toString)
 }

d65e8ed7c29ef06a74531cf9516339b1c4fe0bce
Björn Harrtell <bjorn@wololo.org>
2015-07-14 22:54:30 +0200

    Change order

--- src/main/scala/org/wololo/vectorvictor/cli.scala
+++ src/main/scala/org/wololo/vectorvictor/cli.scala
@@ -31,8 +31,8 @@ object cli extends App with LazyLogging {
       SourceMeta("osm.land_polygons_z5_3006", grid1, 2, Some(1024), None),
       SourceMeta("osm.land_polygons_z8_3006", grid1, 4, Some(128), Some(1024)),
       SourceMeta("osm.land_polygons_3006", grid2, 8, None, Some(128)),
-      SourceMeta("lantmateriet.al_riks", grid2, 6, None, Some(256)),
-      SourceMeta("lantmateriet.vl_riks", grid2, 7, None, Some(256))
+      SourceMeta("lantmateriet.vl_riks", grid2, 7, None, Some(256)),
+      SourceMeta("lantmateriet.al_riks", grid2, 6, None, Some(256))
     )
     
     val cacheMetas = sourceMetas.map(sourceMeta => makeTileCache(sourceMeta))
